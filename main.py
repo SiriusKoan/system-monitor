767a82bf2f1dd4ed797d4111bb9683e6fd75190b
SiriusKoan <k.peihsun@gmail.com>
2021-12-11 12:42:56 +0800

    Rewrite diskio thread

--- main.py
+++ main.py
@@ -39,3 +39,5 @@ while True:
         print(timestamps)
     elif command == "exit":
         break
+    else:
+        print("Command not found")
--- monitor/diskio.py
+++ monitor/diskio.py
@@ -1,6 +1,5 @@
 from time import sleep
 import psutil
-from psutil._common import bytes2human
 from monitor.config import DISK_INTERVAL
 
 disk_read_speed_total = []
@@ -11,7 +10,6 @@ disk_write_speed_per = {device: [] for device in devices}
 
 
 def _get_disk_io_speed():
-    # not precise for unknown reasons
     disk_read_speed_total.append(psutil.disk_io_counters().read_bytes)
     disk_write_speed_total.append(psutil.disk_io_counters().write_bytes)
     for device in devices:
@@ -21,50 +19,34 @@ def _get_disk_io_speed():
         disk_write_speed_per[device].append(
             psutil.disk_io_counters(perdisk=True)[device].write_bytes
         )
-    sleep(DISK_INTERVAL)
-    disk_read_speed_total.append(
-        psutil.disk_io_counters().read_bytes - disk_read_speed_total[0]
-    )
-    disk_write_speed_total.append(
-        psutil.disk_io_counters().write_bytes - disk_write_speed_total[0]
-    )
-    for device in devices:
-        disk_read_speed_per[device].append(
-            psutil.disk_io_counters(perdisk=True)[device].read_bytes
-            - disk_read_speed_per[device][0]
-        )
-        disk_write_speed_per[device].append(
-            psutil.disk_io_counters(perdisk=True)[device].write_bytes
-            - disk_write_speed_per[device][0]
-        )
-    sleep(DISK_INTERVAL)
     while True:
         disk_read_speed_total.append(
-            psutil.disk_io_counters().read_bytes
-            - disk_read_speed_total[-1]
-            - disk_read_speed_total[0]
+            psutil.disk_io_counters().read_bytes - disk_read_speed_total[0]
         )
         disk_write_speed_total.append(
-            psutil.disk_io_counters().write_bytes
-            - disk_write_speed_total[-1]
-            - disk_write_speed_total[0]
+            psutil.disk_io_counters().write_bytes - disk_write_speed_total[0]
         )
         for device in devices:
             disk_read_speed_per[device].append(
                 psutil.disk_io_counters(perdisk=True)[device].read_bytes
-                - disk_read_speed_per[device][-1]
                 - disk_read_speed_per[device][0]
             )
             disk_write_speed_per[device].append(
                 psutil.disk_io_counters(perdisk=True)[device].write_bytes
-                - disk_write_speed_per[device][-1]
                 - disk_write_speed_per[device][0]
             )
         sleep(DISK_INTERVAL)
 
 
 def get_disk_io_speed():
-    return (
-        [bytes2human(speed) for speed in disk_read_speed_total[1:]],
-        [bytes2human(speed) for speed in disk_write_speed_total[1:]],
-    )
+    read = [disk_read_speed_total[1]]
+    read += [
+        disk_read_speed_total[index] - disk_read_speed_total[index - 1]
+        for index in range(2, len(disk_read_speed_total))
+    ]
+    write = [disk_write_speed_total[1]]
+    write += [
+        disk_write_speed_total[index] - disk_write_speed_total[index - 1]
+        for index in range(2, len(disk_write_speed_total))
+    ]
+    return read, write

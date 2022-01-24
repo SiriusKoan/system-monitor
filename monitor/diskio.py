4ae9669f8245a30368fee399174f7ee986bec0e0
SiriusKoan <k.peihsun@gmail.com>
2022-01-24 12:34:56 +0800

    Remove per disk and per nic list

--- monitor/diskio.py
+++ monitor/diskio.py
@@ -5,20 +5,20 @@ from monitor.config import TIME_INTERVAL
 disk_read_speed_total = []
 disk_write_speed_total = []
 devices = psutil.disk_io_counters(perdisk=True).keys()
-disk_read_speed_per = {device: [] for device in devices}
-disk_write_speed_per = {device: [] for device in devices}
+# disk_read_speed_per = {device: [] for device in devices}
+# disk_write_speed_per = {device: [] for device in devices}
 
 
 def _get_disk_io_speed():
     disk_read_speed_total.append(psutil.disk_io_counters().read_bytes)
     disk_write_speed_total.append(psutil.disk_io_counters().write_bytes)
-    for device in devices:
-        disk_read_speed_per[device].append(
-            psutil.disk_io_counters(perdisk=True)[device].read_bytes
-        )
-        disk_write_speed_per[device].append(
-            psutil.disk_io_counters(perdisk=True)[device].write_bytes
-        )
+    # for device in devices:
+    #     disk_read_speed_per[device].append(
+    #         psutil.disk_io_counters(perdisk=True)[device].read_bytes
+    #     )
+    #     disk_write_speed_per[device].append(
+    #         psutil.disk_io_counters(perdisk=True)[device].write_bytes
+    #     )
     while True:
         disk_read_speed_total.append(
             psutil.disk_io_counters().read_bytes - disk_read_speed_total[0]
@@ -26,15 +26,15 @@ def _get_disk_io_speed():
         disk_write_speed_total.append(
             psutil.disk_io_counters().write_bytes - disk_write_speed_total[0]
         )
-        for device in devices:
-            disk_read_speed_per[device].append(
-                psutil.disk_io_counters(perdisk=True)[device].read_bytes
-                - disk_read_speed_per[device][0]
-            )
-            disk_write_speed_per[device].append(
-                psutil.disk_io_counters(perdisk=True)[device].write_bytes
-                - disk_write_speed_per[device][0]
-            )
+        # for device in devices:
+        #     disk_read_speed_per[device].append(
+        #         psutil.disk_io_counters(perdisk=True)[device].read_bytes
+        #         - disk_read_speed_per[device][0]
+        #     )
+        #     disk_write_speed_per[device].append(
+        #         psutil.disk_io_counters(perdisk=True)[device].write_bytes
+        #         - disk_write_speed_per[device][0]
+        #     )
         sleep(TIME_INTERVAL)
 
 
@@ -49,20 +49,20 @@ def get_disk_io_speed(per=False):
         disk_write_speed_total[index] - disk_write_speed_total[index - 1]
         for index in range(2, len(disk_write_speed_total))
     ]
-    read_per = {device: [disk_read_speed_per[device][1]] for device in devices}
-    write_per = {device: [disk_write_speed_per[device][1]] for device in devices}
-    for device in devices:
-        read_per[device] += [
-            disk_read_speed_per[device][index] - disk_read_speed_per[device][index - 1]
-            for index in range(2, len(disk_read_speed_per[device]))
-        ]
-        write_per[device] += [
-            disk_write_speed_per[device][index]
-            - disk_write_speed_per[device][index - 1]
-            for index in range(2, len(disk_write_speed_per[device]))
-        ]
-    if per:
-        return read_per, write_per
+    # read_per = {device: [disk_read_speed_per[device][1]] for device in devices}
+    # write_per = {device: [disk_write_speed_per[device][1]] for device in devices}
+    # for device in devices:
+    #     read_per[device] += [
+    #         disk_read_speed_per[device][index] - disk_read_speed_per[device][index - 1]
+    #         for index in range(2, len(disk_read_speed_per[device]))
+    #     ]
+    #     write_per[device] += [
+    #         disk_write_speed_per[device][index]
+    #         - disk_write_speed_per[device][index - 1]
+    #         for index in range(2, len(disk_write_speed_per[device]))
+    #     ]
+    # if per:
+        # return read_per, write_per
     return read, write
 
 def clear_disk_io_speed():
@@ -72,10 +72,10 @@ def clear_disk_io_speed():
     disk_write_speed_total.clear()
     disk_read_speed_total.append(read_total_tmp)
     disk_write_speed_total.append(write_total_tmp)
-    for device in devices:
-        read_per_tmp = disk_read_speed_per[device][-1]
-        write_per_tmp = disk_write_speed_per[device][-1]
-        disk_read_speed_per[device].clear()
-        disk_write_speed_per[device].clear()
-        disk_read_speed_per[device].append(read_per_tmp)
-        disk_write_speed_per[device].append(write_per_tmp)
+    # for device in devices:
+    #     read_per_tmp = disk_read_speed_per[device][-1]
+    #     write_per_tmp = disk_write_speed_per[device][-1]
+    #     disk_read_speed_per[device].clear()
+    #     disk_write_speed_per[device].clear()
+    #     disk_read_speed_per[device].append(read_per_tmp)
+    #     disk_write_speed_per[device].append(write_per_tmp)
--- monitor/networkio.py
+++ monitor/networkio.py
@@ -12,13 +12,13 @@ network_output_speed_per = {interface: [] for interface in interfaces}
 def _get_network_io_speed():
     network_input_speed_total.append(psutil.net_io_counters().bytes_recv)
     network_output_speed_total.append(psutil.net_io_counters().bytes_sent)
-    for interface in interfaces:
-        network_input_speed_per[interface].append(
-            psutil.net_io_counters(pernic=True)[interface].bytes_recv
-        )
-        network_output_speed_per[interface].append(
-            psutil.net_io_counters(pernic=True)[interface].bytes_sent
-        )
+    # for interface in interfaces:
+    #     network_input_speed_per[interface].append(
+    #         psutil.net_io_counters(pernic=True)[interface].bytes_recv
+    #     )
+    #     network_output_speed_per[interface].append(
+    #         psutil.net_io_counters(pernic=True)[interface].bytes_sent
+    #     )
     while True:
         network_input_speed_total.append(
             psutil.net_io_counters().bytes_recv - network_input_speed_total[0]
@@ -26,15 +26,15 @@ def _get_network_io_speed():
         network_output_speed_total.append(
             psutil.net_io_counters().bytes_sent - network_output_speed_total[0]
         )
-        for interface in interfaces:
-            network_input_speed_per[interface].append(
-                psutil.net_io_counters(pernic=True)[interface].bytes_recv
-                - network_input_speed_per[interface][0]
-            )
-            network_output_speed_per[interface].append(
-                psutil.net_io_counters(pernic=True)[interface].bytes_sent
-                - network_output_speed_per[interface][0]
-            )
+        # for interface in interfaces:
+        #     network_input_speed_per[interface].append(
+        #         psutil.net_io_counters(pernic=True)[interface].bytes_recv
+        #         - network_input_speed_per[interface][0]
+        #     )
+        #     network_output_speed_per[interface].append(
+        #         psutil.net_io_counters(pernic=True)[interface].bytes_sent
+        #         - network_output_speed_per[interface][0]
+        #     )
         sleep(TIME_INTERVAL)
 
 
@@ -49,40 +49,42 @@ def get_network_io_speed(per=False):
         network_output_speed_total[index] - network_output_speed_total[index - 1]
         for index in range(2, len(network_output_speed_total))
     ]
-    input_per = {
-        interface: [network_input_speed_per[interface][1]] for interface in interfaces
-    }
-    output_per = {
-        interface: [network_output_speed_per[interface][1]] for interface in interfaces
-    }
-    for interface in interfaces:
-        input_per[interface] += [
-            network_input_speed_per[interface][index]
-            - network_input_speed_per[interface][index - 1]
-            for index in range(2, len(network_input_speed_per[interface]))
-        ]
-        output_per[interface] += [
-            network_output_speed_per[interface][index]
-            - network_output_speed_per[interface][index - 1]
-            for index in range(2, len(network_output_speed_per[interface]))
-        ]
-    if per:
-        return input_per, output_per
+    # input_per = {
+    #     interface: [network_input_speed_per[interface][1]] for interface in interfaces
+    # }
+    # output_per = {
+    #     interface: [network_output_speed_per[interface][1]] for interface in interfaces
+    # }
+    # for interface in interfaces:
+    #     input_per[interface] += [
+    #         network_input_speed_per[interface][index]
+    #         - network_input_speed_per[interface][index - 1]
+    #         for index in range(2, len(network_input_speed_per[interface]))
+    #     ]
+    #     output_per[interface] += [
+    #         network_output_speed_per[interface][index]
+    #         - network_output_speed_per[interface][index - 1]
+    #         for index in range(2, len(network_output_speed_per[interface]))
+    #     ]
+    # if per:
+    #     return input_per, output_per
     return input_total, output_total
 
 
 def clear_network_io_speed():
+    input_total_tmp = network_input_speed_total[-1]
+    output_total_tmp = network_output_speed_total[-1]
     network_input_speed_total.clear()
     network_output_speed_total.clear()
-    for interface in interfaces:
-        network_input_speed_per[interface].clear()
-        network_output_speed_per[interface].clear()
-    network_input_speed_total.append(psutil.net_io_counters().bytes_recv)
-    network_output_speed_total.append(psutil.net_io_counters().bytes_sent)
-    for interface in interfaces:
-        network_input_speed_per[interface].append(
-            psutil.net_io_counters(pernic=True)[interface].bytes_recv
-        )
-        network_output_speed_per[interface].append(
-            psutil.net_io_counters(pernic=True)[interface].bytes_sent
-        )
+    # for interface in interfaces:
+    #     network_input_speed_per[interface].clear()
+    #     network_output_speed_per[interface].clear()
+    network_input_speed_total.append(input_total_tmp)
+    network_output_speed_total.append(output_total_tmp)
+    # for interface in interfaces:
+    #     network_input_speed_per[interface].append(
+    #         psutil.net_io_counters(pernic=True)[interface].bytes_recv
+    #     )
+    #     network_output_speed_per[interface].append(
+    #         psutil.net_io_counters(pernic=True)[interface].bytes_sent
+    #     )
